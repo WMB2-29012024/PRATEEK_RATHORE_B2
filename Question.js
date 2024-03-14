b7297eef1b0b48fcca4eba60fcf2f759e8c41388
Prateek9340 <prateeksrathore111@gmail.com>
2024-03-14 16:52:18 +0530

    nextGreaterElement solve

--- Question.js
+++ Question.js
@@ -1,42 +1,4 @@
-
-class Stack {
-    constructor(size) {
-        this.items = [];
-        this.maxsize = size;
-        this.currentsize = 0;
-    }
-    
-    stackPush(elem) {
-        if (this.currentsize >= this.maxsize) {
-            return "stack is full";
-        } else {
-            this.items.push(elem);
-            this.currentsize += 1;
-            return this.currentsize;
-        }
-    }
-    
-    stackPop() {
-        if (this.currentsize === 0) {
-            return new Error("stack underflow");
-        }
-        this.currentsize -= 1;
-        return this.items.pop();
-    }
-    
-    peek() {
-        if (this.currentsize === 0) {
-            return "Stack is empty";
-        } else {
-            const lastelm = this.items[this.currentsize - 1];
-            return lastelm;
-        }
-    }
-    
-    isEmpty() {
-        return this.currentsize === 0;
-    }
-}
+ 
 // Example 1:
 // Input: [4, 5, 2, 25]
 // Output: [5, 25, 25, -1]
@@ -44,23 +6,85 @@ class Stack {
 // Example 2:
 // Input: [13, 7, 6, 12]
 // Output: [-1, 12, 12, -1]
+class Stack {
 
-let nums= [4, 5, 2, 25]
-
-const Nxt_Greater_Element =(nums) =>{
-    const mystack=new Stack(nums);
-    let newarr=[]
-     for(let i=nums.length-1;i>=0;i--){
-     let new_num=nums[i];
-     while(!mystack.isEmpty() && new_num>=mystack.peek()){
-     mystack.stackPop()
+    constructor(size) {
+        this.stack = [];
+        this.maxsize = size;
+        this.currentSize = 0;
     }
-   if(mystack.isEmpty()){
-    newarr[i] =-1
-   } else{
-    newarr[i]=mystack.peek();
-   }
+
+    StackPush(elem) {
+        if (this.currentSize >= this.maxsize) {
+            return "Stack is full";
+        } else {
+            this.stack.push(elem);
+            this.currentSize += 1;
+            return this.currentSize
+
+        }
+    }
+    StackPop() {
+        if (this.currentSize <= 0) {
+            return "Stack is empty";
+        } else {
+            this.popElem = this.stack.pop();
+            this.currentSize -= 1;
+            return this.popElem
+        }
+    }
+    IsEmpty() {
+        if (this.currentSize <= 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    OnTop() {
+        if (this.currentSize <= 0) {
+            return "Stack is empty";
+        } else {
+            this.TopElem = this.stack.pop();
+            this.stack.push(this.TopElem);
+            return this.TopElem
+        }
     }
 
 }
-Nxt_Greater_Element(nums)
+const arr = [4, 5, 2, 25];
+
+const nextGreaterElement = (input) => {
+
+    const myStack = new Stack(input);
+
+    let newArr = [];
+
+    for (let i = (input.length - 1); i >= 0; i--) {
+
+        const elem = input[i];
+
+        while (!myStack.IsEmpty() && elem > myStack.OnTop()) {
+            myStack.StackPop()
+
+
+        }
+        
+        if (myStack.IsEmpty()) {
+            newArr[i] = -1
+        } else {
+            newArr[i] = myStack.OnTop();
+        }
+        myStack.StackPush(elem);
+
+
+
+
+
+
+
+
+    }
+    return newArr
+};
+console.log(nextGreaterElement(arr))
+ 
