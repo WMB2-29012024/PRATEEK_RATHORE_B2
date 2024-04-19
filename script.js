4a305e306df605f491cea5406f774a907bd3b831
Prateek9340 <prateeksrathore111@gmail.com>
2024-04-19 18:35:04 +0530

    task complete

---
+++ script.js
@@ -0,0 +1,10 @@
+const hameburger=document.querySelector(".hameburger")
+const aside=document.querySelector("aside")
+
+
+hameburger.addEventListener("click",()=>{
+     hameburger.innerHTML="✕"
+     hameburger.style.fontSize="30px"
+     hameburger.style.color="white"
+   
+})
