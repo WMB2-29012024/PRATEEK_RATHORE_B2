150305dbcd6f39480b8f30c47ec34ae867779bdc
Prateek9340 <prateeksrathore111@gmail.com>
2024-04-22 14:30:02 +0530

    no change

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const hameburger=document.querySelector(".hameburger")
-const aside=document.querySelector("aside")
+const aside=document.querySelector(".aside")
+let flag=0;
+ 
 
-
-hameburger.addEventListener("click",()=>{
-     hameburger.innerHTML="✕"
-     hameburger.style.fontSize="30px"
-     hameburger.style.color="white"
+    hameburger.addEventListener("click",()=>{
+        if(flag===0){
+            aside.style.width="290px"
+            hameburger.innerHTML="✕"
+            hameburger.style.fontSize="30px"
+            hameburger.style.color="white"
+          flag=1;
+        }else{            
+               aside.style.width="0"
+               hameburger.innerHTML="☰"
+               flag=0
+        }
    
 })
+ 
