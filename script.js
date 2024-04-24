5e233ae7cb73b1f6fffffd37d5a236c5ceb8543c
Prateek9340 <prateeksrathore111@gmail.com>
2024-04-24 17:22:52 +0530

    linkedin

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-// Get reference to the input and image element
+import { photoUploader } from "./uploadPhoto.js";
+
 const editBtnElem = document.getElementById('editBtn');
 const imagePreview = document.getElementById('preview');
- const mainPopUpDisplay=document.getElementsByClassName("mainPopUpDisplay")
- const mainProfile=document.getElementsByClassName("mainProfile")
+const mainPopUpDisplay = document.querySelector(".mainPopUpDisplay");
+const mainProfile = document.querySelector(".mainProfile");
+let userImg = document.querySelector('#user-img')
 
- mainProfile.addEventListener("click", (e) => {
-    mainPopUpDisplay.style.display = "initial";
+const userPreviewImg = document.querySelector('#user-preview-img')
+const userProfileInputElem = document.querySelector('#user-profile-input')
+editBtnElem.addEventListener('change', function () {
+    photoUploader(imagePreview, this.files);
 });
 
+mainProfile.addEventListener('click', () => {
+    mainPopUpDisplay.style.display = 'initial';
 
-editBtnElem.addEventListener('change', function () {
-    const file = this.files[0]; // Get the selected file
-    if (file) {
-        const reader = new FileReader(); // Initialize a FileReader object
-       
-        reader.onload = function (e) {
-            imagePreview.style.display="initial"
-            // Set the source of the image element to the result of FileReader
-            imagePreview.src = e.target.result;
-            // Reset the progress bar after loading is complete
-            
-        };
-        // Read the selected file as a Data URL
-        reader.readAsDataURL(file);
-    } else {
-        // If no file is selected, display a default image
-        imagePreview.src = '#';
-       
-    }
+});
+
+userProfileInputElem.addEventListener('change', function () {
+    photoUploader(userPreviewImg, this.files);
+    userImg.src = userPreviewImg.src
+
 });
